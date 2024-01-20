46a4006cb0b1b9143b33ea0f0604e35d6bb31d51
unknown <danielabmp26gmail.com>
2024-01-20 09:20:49 -0500

    Firts commit basic_calc script:features calc v.1.0 and v.1.1

---
+++ basic_calc.py
@@ -0,0 +1,39 @@
+
+#script title: Basic Calculator: V1
+'''Developer name: Daniela Rivera
+Date: 18-01-2024 '''
+
+
+'''DESCRIPTION:
+this calculator let you get the result of math basic operations from two numbers(variables)'''
+
+# INPUTS (two variables)
+num1= 20
+num2 = 5
+
+
+#PROCESS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2 
+
+#OUTPUTS
+print("::: BASIC CALC V1 :::")
+print ("Number 1:", num1 type (num1))
+print ("Number 2:", num2 type (num2))
+print("the addition is:",add, type (add))
+print("the subtraction is:",sub, type (sub))
+print("the multiplication:",mul, type(mul))
+print("the division is:",div, type (div))
+print("\n")
+
+
+
+print("::: BASIC CALC V2 :::")
+print ("Number 1 is: ", num1))
+print ("Number 2 is: ", num2))
+print("the addition is:",num1)
+print("the subtraction is:",num2)
+print("the multiplication:",)
+print("the division is:",)
