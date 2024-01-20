cf2f417e4d1f031639ee9bde0f016eeb4a706a11
unknown <danielabmp26gmail.com>
2024-01-20 12:06:52 -0500

    first commit data_types.py

---
+++ data_types.py
@@ -0,0 +1,56 @@
+#Tipos de datos en python 
+#python es un lenguaje dinamicamente tipado
+#python es un lenguaje INTERPRETADO **
+
+
+#1 Numericos
+## Enteros 
+num1 = 42
+print("num1:", type(num1))
+## flotantes o decimales (reales)
+num2 = 50.5
+print("num2:", type(num2))
+#complejos => complex
+num3 = 2j
+print ("num3:",type(num3))
+
+
+
+#2 Cadena string => str
+my_name ="Daniela"
+print("my_name", type(my_name))
+my_lastname = "Rivera"
+print("my_lastname", type(my_lastname))
+address = 'mzc casa17 - cod post 52001'
+profile ='''Daniela es una persona admirable'''
+print("address", type(address))
+print("profile", type(profile))
+
+
+
+a = 1
+b = 1
+suma1 = a + b
+
+c = "1"
+d = "1"
+suma2 = c +d
+
+print("suma1", suma1)
+print("suma2", suma2)
+
+#3 Logicos => Valores o expresiones boleanas (bool)
+usuario_activo = True
+print("usuario_activo", type(usuario_activo))
+pago_realizado = False 
+print("pago_realizado", type(pago_realizado))
+
+#4 listas => list
+frutas = ['Apple', 'Banana']
+print("frutas", type(frutas))
+colors = ['blue', 'brown', 'yellow']
+print("colors", type(colors))
+#5 Duplas
+#6 Diccionarios
+#7 Conjuntos
+
